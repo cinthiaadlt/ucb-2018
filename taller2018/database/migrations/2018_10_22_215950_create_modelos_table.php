0b84448c1d44cfe80456c243b024a59fddd280d8
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-10-22 18:07:25 -0400

    migrate: modelos

---
+++ taller2018/database/migrations/2018_10_22_215950_create_modelos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modelos', function (Blueprint $table) {
+            $table->increments('id_modelos');
+            $table->string('modelo',45);
+            $table->unsignedInteger('anio');
+            $table->unsignedInteger('cat_marca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modelos');
+    }
+}
