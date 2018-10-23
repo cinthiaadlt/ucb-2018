b4f129348ea7df175ab238ef7ed75614f0c398b8
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-10-23 01:21:32 -0400

    migraciones de las tablas privilegios, roles y privilegios_roles.

---
+++ taller2018/database/migrations/2018_10_23_045608_create_privilegios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrivilegiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('privilegios', function (Blueprint $table) {
+            $table->increments('id_privilegios');
+            $table->string('nombre_privileo','45');
+            $table->string('descripcion','250');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('privilegios');
+    }
+}
---
+++ taller2018/database/migrations/2018_10_23_050223_create_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id_roles');
+            $table->string('nombre_rol','45');
+            $table->string('descripcion_rol','250');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
