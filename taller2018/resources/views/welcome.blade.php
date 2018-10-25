b97dcc1e5bf5d7756a088856108c3f8508c3ba6f
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-10-25 13:38:05 -0400

    Prueba cambio

--- taller2018/resources/views/welcome.blade.php
+++ taller2018/resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 1245555
                 </div>
 
                 <div class="links">
