e27000497ccb7d4be4e2434ed68c78ad5d52ad15
DrunkOldDog <juanisoul@gmail.com>
2018-11-07 00:59:29 -0400

    Adicion dimensiones con js en parqueos

--- taller2018/resources/views/parqueo/create.blade.php
+++ taller2018/resources/views/parqueo/create.blade.php
@@ -1,4 +1,7 @@
 @extends('layout.principal')
+@section('scripts')
+<script src="{{asset('_js/parq.js')}}"></script>
+@endsection
 @section('content')
 <link href="{{asset('css/parque.css')}}" rel="stylesheet">
 <div id="content">
@@ -57,6 +60,45 @@
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label class="form-control-label" for="input-parqueo">Dimensiones:</label>
+    <div class="row form-group product-chooser">
+    
+    	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+    		<div class="product-chooser-item selected">
+    			<img src="http://renswijnmalen.nl/bootstrap/desktop_mobile.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile and Desktop">
+                <div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+    				<span class="title">Mobile and Desktop</span>
+    				<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
+    				<input type="radio" name="product" value="mobile_desktop" checked="checked">
+    			</div>
+    			<div class="clear"></div>
+    		</div>
+    	</div>
+    	
+    	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+    		<div class="product-chooser-item">
+    			<img src="http://renswijnmalen.nl/bootstrap/desktop.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Desktop">
+                <div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+    				<span class="title">Desktop</span>
+    				<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
+    				<input type="radio" name="product" value="desktop">
+    			</div>
+    			<div class="clear"></div>
+    		</div>
+    	</div>
+    	
+    	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+    		<div class="product-chooser-item">
+    			<img src="http://renswijnmalen.nl/bootstrap/mobile.png" class="img-rounded col-xs-4 col-sm-4 col-md-12 col-lg-12" alt="Mobile">
+                <div class="col-xs-8 col-sm-8 col-md-12 col-lg-12">
+    				<span class="title">Mobile</span>
+    				<span class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</span>
+    				<input type="radio" name="product" value="mobile">
+    			</div>
+    			<div class="clear"></div>
+    		</div>
+    	</div>
+    	
+    </div>
                                 </div>
                             </div>
 
