b752850bd1031635f9d459a80d3a5e5b456aa358
raulgarafulic <rgarafulicm@gmail.com>
2018-11-09 10:46:28 -0400

    Web.php sin espacios

--- taller2018/routes/web.php
+++ taller2018/routes/web.php
@@ -1,35 +1,12 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::resource('/','ClienteController')->middleware('auth');
-// Route::resource('login','LoginController');
-// Route::resource('signup','SignupController');
-
 Route::resource('vehiculo','VehiculoController')->middleware('auth');
 Route::get('/list','VehiculoController@list');
-
 Route::resource ('/tipoMultas', 'TipoMultaController')->middleware('auth');
-
 Route::resource('parqueos','ParqueoController')->middleware('auth');
-
 Route::resource('usuarios', 'UsuarioController')->middleware('auth');
-
 Route::resource('zona','ZonaController')->middleware('auth');
-
 Route::resource('denuncia','DenunciaController')->middleware('auth');
-
 Route::resource('cliente_busqueda','ClienteController')->middleware('auth');
-
 Route::get('/gmaps', ['as ' => 'gmaps', 'uses' => 'GmapsController@index'])->middleware('auth');//Ruta de prueba no eliminar
-
 Auth::routes ();
