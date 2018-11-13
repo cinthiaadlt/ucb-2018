12e41328bc53da3c467844a014211a3eb1e61aeb
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-11-13 18:15:16 -0400

    Cambio en el controlador del cliente
    Se descarto el codigo de la anterior libreria y ahora solo se realiza la consulta a la BD y luego se manda la variable a la vista para luego ser utilizada y mostrar los marcadores de los parqueos en el mapa

--- taller2018/app/Http/Controllers/ClienteController.php
+++ taller2018/app/Http/Controllers/ClienteController.php
@@ -15,49 +15,10 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //configuaración
-        $config = array();
-        $config['center'] = 'auto';
-        $config['map_width'] = 869;
-        $config['map_height'] = 600;
-        $config['zoom'] = 15;
-        $config['onboundschanged'] = 'if (!centreGot) {
-            var mapCentre = map.getCenter();
-            marker_0.setOptions({
-                position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng())
 
-            });
-        }
-        centreGot = true;';
+        $locations = DB::table('parqueos')->get();
+        return view('cliente.busqueda_parqueo',compact('locations'));
 
-        \Gmaps::initialize($config);
-
-        // Colocar el marcador
-        // Una vez se conozca la posición del usuario
-
-        $marker = array();
-        $marker['position'] = 'auto';
-        $marker['infowindow_content'] = 'Posicion Actual';
-        \Gmaps::add_marker($marker);
-
-        $marker = array();
-        $marker['position'] = '-16.498605, -68.123590';
-        $marker['infowindow_content'] = 'Hello World!';
-        $marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|9999FF|000000';
-        \Gmaps::add_marker($marker);
-
-        $marker = array();
-        $location = DB::table('parqueos')->select(['latitud_x','longitud_y'])->where('id_parqueos', '=', '1')->get();
-        $ubicacion = json_encode($location);
-        $marker['position'] = $ubicacion;
-        $marker['infowindow_content'] = 'Hello World!';
-        $marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|9999FF|000000';
-        \Gmaps::add_marker($marker);
-
-        $map = \Gmaps::create_map();
-
-        //Devolver vista con datos del mapa
-        return view('cliente.busqueda_parqueo',compact('map'));
     }
 
     /**
