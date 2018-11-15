55bc4ebbd52d8190084aaa4e84b64fb1ab557323
DrunkOldDog <juanisoul@gmail.com>
2018-11-15 19:31:07 -0400

    Arreglo en UsersTableSeeder para los 3 tipos de usuario referidos al desarrollo del proyecto

--- taller2018/database/seeds/UsersTableSeeder.php
+++ taller2018/database/seeds/UsersTableSeeder.php
@@ -14,24 +14,25 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             // Para que los tres tipos de roles puedan darse,
             // Poner php artisan migrate:fresh --seed para hacer la migración del seed
-            //  php artisan db:seed --class=RolesTableSeeder
+            // php artisan db:seed --class=RolesTableSeeder
+            // y luego php artisan db:seed --class=UsersTableSeeder
           [
             'role_id' => '3',
             'name' => 'Juan Reyes',
             'email' => 'rata_reyes1@hotmail.com',
-            'password' => 'hola123',
+            'password' => Hash::make('hola123'),
           ],
           [
             'role_id' => '2',
             'name' => 'Cinthia Alvarez',
             'email' => 'cinthia1@gmail.com',
-            'password' => 'hola123',
+            'password' => Hash::make('hola123'),
           ],
           [
             'role_id' => '1',
             'name' => 'Diego Rocha',
             'email' => 'dsrr2r@gmail.com',
-            'password' => '111111',
+            'password' => Hash::make('111111'),
           ]
         ]);
     }
