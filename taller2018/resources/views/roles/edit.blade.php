47127e55956efeed774632aa1543f0681c045bae
raulgarafulic <rgarafulicm@gmail.com>
2018-11-16 17:27:40 -0400

    Modificaciones para Roles

--- taller2018/resources/views/roles/edit.blade.php
+++ taller2018/resources/views/roles/edit.blade.php
@@ -1,17 +1,60 @@
-@extends ('layouts.app')
+@extends('layout.principal')
 
-@section ('content')
-<h1>Edit</h1>
-<form method="post" action="{{ route('role.update', $role->id_roles) }}">
-  {{csrf_field()}}
-  <input type="hidden" name="_method" value="PUT">
-  <label for="">nombre_role</label>
-  <input type="text" name="nombre_role" value="{{$role->nombre_role}}">
-  <label for="">descripcion_role</label>
-  <input type="text" name="descripcion_role" value="{{$role->descripcion_role}}">
-  <input type="submit" name="submit">
-  <form method="post" action="/role/{{$role->id_roles}}">
-    <input type="hidden" name="_method" value="delete">
-    <input type="submit" value="delete">
-  </form>
-</form>
+@section('content')
+<div id="content">
+  <div class="container-fluid mt--7">
+    <div class="row">
+      <div class="col-xl-12 order-xl-1">
+        <div class="card bg-secondary shadow">
+
+          @if (count($errors) > 0)
+            <div class="alert alert-danger">
+              <strong>Error!</strong> Revise los campos obligatorios.<br><br>
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+
+          @if(Session::has('success'))
+            <div class="alert alert-info">
+              {{Session::get('success')}}
+            </div>
+          @endif
+
+          <div class="card-header bg-white border-0">
+            <div class="row align-items-center">
+              <div class="col-8">
+                <h3 class="mb-0">Edición de Roles</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <form method="POST" action="{{ route('roles.update', $role->id_roles) }}" role="form" class="panel-body" style="padding-bottom:30px;">
+              {{ csrf_field() }}
+              <input name="_method" type="hidden" value="PATCH">
+              <div class="col-lg-12">
+                <div class="form-group">
+                  <label class="form-control-label" for="nombre_rol">Nombre</label>
+                  <input type="text" name="nombre_rol" id="nombre_rol" class="form-control form-control-alternative" value = "{{ $role->nombre_rol}}">
+                </div>
+              </div>
+
+              <div class="col-lg-12">
+                <div class="form-group">
+                  <label class="form-control-label" for="descripcion_rol">Descripción</label>
+                  <input type="text" name="descripcion_rol" id="descripcion_rol" class="form-control form-control-alternative" value = "{{ $role->descripcion_rol}}">
+                </div>
+              </div>
+              <input class="submit btn btn-danger" type="submit" value="Submit">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
