9a6e5d56f4c03f947fbb7032110e4e1b9412c9a9
raulgarafulic <rgarafulicm@gmail.com>
2018-11-21 07:55:18 -0400

    Descomenté seeders que estaban comentados desde un push mío.

--- taller2018/database/seeds/DatabaseSeeder.php
+++ taller2018/database/seeds/DatabaseSeeder.php
@@ -16,16 +16,16 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class); Por defecto ejemplo
 
         // Se necesita llenar usuarios, roles, users_roles, dias especificos
-        //$this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
         $this->call(RolesTableSeeder::class);
 
-        //$this->call(Users_RolesTableSeeder::class);
+        $this->call(Users_RolesTableSeeder::class);
 
         $this->call(DiasTableSeeder::class);
 
         $this->call(ZonasTableSeeder::class);
-        
+
         //Se crean las zonas, parqueos con la clase Factory
         //factory(Zona::class,7)->create();
         factory(Parqueo::class, 3)->create();
