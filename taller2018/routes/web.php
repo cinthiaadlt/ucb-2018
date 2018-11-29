ef2f88f9b4d5b65039f939ad8f4fa4f84f12b1fb
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-11-29 03:34:40 -0400

    se agrego la ruta de parqueos favoritos

--- taller2018/routes/web.php
+++ taller2018/routes/web.php
@@ -33,6 +33,8 @@ Auth::routes ();
     Route::resource('cliente_busqueda','ClienteController')->middleware('auth');
     Route::get('/lista_reservas','ReservaController@lista_estado');//nueva
     Route::get('mostrar_reserva','ReservaController@edit');//nueva
+    Route::get('parqueos_favoritos','FavoritosController@list');//nueva
+
 
 
 
