141b08fd31e1b9cc38fce65c562b328ff1100833
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-11-28 23:25:12 -0400

    arreglo en las rutas

--- taller2018/routes/web.php
+++ taller2018/routes/web.php
@@ -1,45 +1,46 @@
 <?php
 Auth::routes ();
 
-// Rutas de Admin:
-Route::resource ('/tipoMultas', 'TipoMultaController')->middleware('auth');
-Route::resource('/roles','RoleController', ['middleware'=>['auth', 'admin']]);
-Route::resource('/users','UsersRoleController', ['middleware'=>['auth', 'admin']]);
-Route::resource('parqueo_admin','ParqueoAdminController',['middleware' => ['auth', 'admin']]);
-Route::resource('denuncia','DenunciaController',['middleware' => ['auth', 'admin', 'user']]);
-Route::resource('validacion','ValidacionController')->middleware('auth');
+// Rutas de ADMINISTRADOR:
+    Route::resource ('/tipoMultas', 'TipoMultaController')->middleware('auth');
+    Route::resource('/roles','RoleController', ['middleware'=>['auth', 'admin']]);
+    Route::resource('/users','UsersRoleController', ['middleware'=>['auth', 'admin']]);
+    Route::resource('parqueo_admin','ParqueoAdminController',['middleware' => ['auth', 'admin']]);
+    Route::resource('denuncia','DenunciaController',['middleware' => ['auth', 'admin', 'user']]);
+    Route::resource('validacion','ValidacionController')->middleware('auth');
 
 
-// Rutas de User;
-Route::resource('/','ClienteController', ['middleware'=>['auth', 'user']]);
-Route::resource('factura','FacturaController', ['middleware'=>['auth', 'user']]);
-Route::get('/reservas/facturacion/{id}',[
-    'uses' => 'FacturaController@showFactura', 
-    'as' => 'test.route'
-], ['middleware'=>['auth', 'user']]);
-Route::get('/reservas/store/{id}',[
-    'uses' => 'ReservaController@store', 
-    'as' => 'testa.route'
-], ['middleware'=>['auth', 'user']]);
-Route::resource('cliente','ClienteController', ['middleware'=>['auth', 'user']]);
-Route::resource('vehiculo','VehiculoController', ['middleware'=>['auth', 'user']]);
-Route::get('makeMeUser','UsersRoleController@makeMeUser', ['middleware'=>['auth']]);
-Route::resource('reservas','ReservaController', ['middleware' => ['auth', 'user']]);
-Route::get('/lista_reservas','ReservaController@lista_estado');//nueva
-Route::get('mostrar_reserva','ReservaController@edit');//nueva
-//Route::get('/parqueos/edit', 'ParqueoController@getParqueoEdit');
-//Route::post('/parqueos/edit', 'ParqueoController@postParqueoEdit');
+// Rutas de CLIENTE O USUARIO;
+    Route::resource('/','ClienteController', ['middleware'=>['auth', 'user']]);
+    Route::resource('factura','FacturaController', ['middleware'=>['auth', 'user']]);
 
-// no estoy seguro:
-Route::get('/list','VehiculoController@list');
+    Route::get('/reservas/facturacion/{id}',[
+        'uses' => 'FacturaController@showFactura',
+        'as' => 'test.route'
+    ], ['middleware'=>['auth', 'user']]);
 
-Route::get('/gmaps', ['as ' => 'gmaps', 'uses' => 'GmapsController@index'])->middleware('auth');//Ruta de prueba no eliminar
-Route::resource('cliente_busqueda','ClienteController')->middleware('auth');
+    Route::get('/reservas/store/{id}',[
+        'uses' => 'ReservaController@store',
+        'as' => 'testa.route'
+    ], ['middleware'=>['auth', 'user']]);
 
-// Rutas de Owner
-Route::resource('zona','ZonaController',['middleware' => ['auth', 'owner']]);
-Route::resource('parqueos','ParqueoController',['middleware' => ['auth', 'owner']]);
-Route::resource('reservasanfitrion','ReservaAnfitrionController')->middleware('auth',['middleware' => ['auth', 'owner']]);
+    Route::resource('cliente','ClienteController', ['middleware'=>['auth', 'user']]);
+    Route::resource('vehiculo','VehiculoController', ['middleware'=>['auth', 'user']]);
+    Route::get('makeMeUser','UsersRoleController@makeMeUser', ['middleware'=>['auth']]);
+    Route::resource('reservas','ReservaController', ['middleware' => ['auth', 'user']]);
+    Route::get('/list','VehiculoController@list', ['middleware' => ['auth', 'user']]);
+    Route::get('vehiculo/{vehiculo}/edit','VehiculoController@edit', ['middleware' => ['auth', 'user']]);
+    Route::resource('cliente_busqueda','ClienteController')->middleware('auth');
+    Route::get('/lista_reservas','ReservaController@lista_estado');//nueva
+    Route::get('mostrar_reserva','ReservaController@edit');//nueva
+
+
+
+
+// Rutas de OWNER
+    Route::resource('zona','ZonaController',['middleware' => ['auth', 'owner']]);
+    Route::resource('parqueos','ParqueoController',['middleware' => ['auth', 'owner']]);
+    Route::resource('reservasanfitrion','ReservaAnfitrionController')->middleware('auth',['middleware' => ['auth', 'owner']]);
 
 // Dejo dos opciones para que veáis cómo implementar los middlewares para cada URL,
 // Cambia ligeramente entre Route::get y Route::resource.
