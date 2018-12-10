014102e7fc1d6ae961deb4c64d6532380598c35f
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-12-10 13:15:59 -0400

    Eliminacion de comentarios

--- taller2018/app/Http/Controllers/ReservaController.php
+++ taller2018/app/Http/Controllers/ReservaController.php
@@ -12,11 +12,7 @@ use Carbon;
 
 class ReservaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
 
@@ -24,11 +20,7 @@ class ReservaController extends Controller
         return view('cliente.busqueda_parqueo',compact('locations'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
@@ -46,12 +38,7 @@ class ReservaController extends Controller
          return view('reserva.historia',compact('reservas','pq2','pq1','date'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $gg=0;
@@ -133,23 +120,13 @@ class ReservaController extends Controller
         return redirect('reservas')->with('success','Reserva Exitosa');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         $vh = Parqueo::find($id);
@@ -191,12 +168,7 @@ class ReservaController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         $reserva = \App\Reserva::find($id);
