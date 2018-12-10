50be2b8b0d42a78a5e16b7e7cbb0b5f827e53d34
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-12-10 18:24:24 -0400

    cambio nombre de boton

--- taller2018/resources/views/parqueo/edit.blade.php
+++ taller2018/resources/views/parqueo/edit.blade.php
@@ -151,7 +151,7 @@
                                 <div class="row">
                                   <div class="col-md-4"></div>
                                   <div class="form-group col-md-4" style="margin-top:60px">
-                                    <button type="submit" class="btn btn-success">Update</button>
+                                    <button type="submit" class="btn btn-success">Actualizar</button>
                                   </div>
                                 </div>
                               </form>
