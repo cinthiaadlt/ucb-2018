feefa74cf338a8829b64aaffcec0b40d483baa77
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-12-09 22:46:57 -0400

    Listado favoritos

--- taller2018/resources/views/cliente/favoritos.blade.php
+++ taller2018/resources/views/cliente/favoritos.blade.php
@@ -9,7 +9,7 @@
                     <div class="card shadow">
 
                         <div class="card-header border-0">
-                            <h3 class="mb-0">Listado Parqueos</h3>
+                            <h3 class="mb-0">Listado Parqueos Favoritos</h3>
                         </div>
 
                         <div class="table-responsive">
@@ -34,22 +34,24 @@
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @if($l->count())
                                 @foreach($l as $fav)
                                     <tr>
 
-                                        <td>{{$fav['id_parqueos_favoritos']}}</td>
-                                        <td>{{$fav['sur_name']}}  {{$fav['las_name']}} </td>
-                                        <td>{{$fav['direccion']}}</td>
-                                        <td><option>{{$fav['hora_apertura']}}</option>
-                                            <option>{{$fav['hora_cierre']}}</option></td>
+                                        <td>{{$fav->id_parqueos_favoritos}}</td>
+                                        <td>{{$fav->sur_name}}  {{$fav->last_name}} </td>
+                                        <td>{{$fav->direccion}}</td>
+                                        <td><option>{{$fav->hora_apertura}}</option>
+                                            <option>{{$fav->hora_cierre}}</option></td>
                                         <?php
                                         $validado = DB::table('precios_alquiler')
                                             ->select('id_dias')
-                                            ->where('id_parqueos', $fav['id_parqueos'])
+                                            ->where('id_parqueos', $fav->id_parqueos)
                                             ->where('estado', true)
                                             ->orderBy('id_precios_alquiler')
                                             ->get();
                                         ?>
+
                                         <td>
                                             @foreach($validado as $val)
                                                 @foreach($dias as $dia)
@@ -60,23 +62,27 @@
                                             @endforeach
                                         </td>
 
-                                        <td><a class="btn btn-dribbble btn-sm" href="{{route('reservas.edit', $fav['id_parqueos'])}}"  >
+                                        <td><a class="btn btn-dribbble btn-sm" href="{{route('reservas.edit', $fav->id_parqueos)}}"  >
                                                 <i class="ni ni-time-alarm"></i> </a></td>
+
                                         <td>
-                                            <form action="{{action('FavoritosController@eliminar', $fav->id_parqueos_favoritos)}}" method="post">
+                                            <form action="{{action('ParqueoFavoritoController@destroy',$fav->id_parqueos_favoritos)}}" method="post">
                                                 @csrf
                                                 <input name="_method" type="hidden" value="DELETE">
-                                                <button class="btn btn-danger" type="submit" onclick="return confirm('¿Desea eliminar el parqueo de favoritos?')">Delete</button>
+                                                <button class="btn btn-danger" type="submit" onclick="return confirm('¿Desea eliminar el parqueo de favoritos?')">Eliminar</button>
                                             </form>
                                         </td>
-
                                     </tr>
                                 @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="8">No hay parqueos favoritos guardados !!</td>
+                                    </tr>
+                                @endif
                                 </tbody>
                             </table>
-
-                            <a href="{{action('ParqueoController@create')}}" class="btn btn-primary">Registro</a>
                         </div>
+                        {{ $l->links() }}
                     </div>
                 </div>
             </div>
