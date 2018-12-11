f9e1a2b0b8ba3d411a07761f5f68554afccddcc3
cinthiaadlt <cinthia.alvarezs3@gmail.com>
2018-12-10 23:58:58 -0400

    Se agrego contenido a la funcion show
    En esta funcion se realiza el query necesario para poder seleccionar desde el boton denunciar desde la tabla de reservas

--- taller2018/app/Http/Controllers/ReservaClienteController.php
+++ taller2018/app/Http/Controllers/ReservaClienteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Denuncia;
+use App\Reserva;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -67,46 +69,32 @@ class ReservaClienteController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        //
+        $d1 = DB::table('parqueos')
+            ->select('*')
+            ->join('users','users.id','=','parqueos.id_users')
+            ->where('id_parqueos',$id)
+            ->get();
+        //dd($d1);
+
+        return view('denuncia.create',compact('d1'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
